c13269eced20ba10fbe3f934e438294eef2123c6
Valentin Troyan <troval12989@gmail.com>
2019-01-30 16:08:04 +0200

    Beta

--- src/App.js
+++ src/App.js
@@ -1,28 +1,65 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-class App extends Component {
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = "a93c1cbd8270f54cbed96f4cba91a3e8";
+
+class App extends React.Component {
+  state = {
+    tempature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  // Arrow functions you allow to use in React version 16+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    // If city & country returns true
+    if(city && country) {
+      console.log(data);
+      this.setState({
+        tempature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        tempature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please, enter the values."
+      });
+    }
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Titles />
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          tempature={this.state.tempature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+          />
       </div>
     );
   }
-}
+};
 
 export default App;
